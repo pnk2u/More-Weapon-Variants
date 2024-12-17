5e2d91e380ce8bf1ad9aa81e8cab45770122ebca
pnk2u <159317511+pnk2u@users.noreply.github.com>
2024-12-17 14:22:59 +0100

    Clean-up TippedArrowRecipeMixin
    fixing bugs related to similar crafting recipes
    (thanks to @Linguardium for helping out here)
    
    Co-Authored-By: Linguardium <61733861+linguardium@users.noreply.github.com>

--- src/main/java/de/pnku/mstv_mweaponv/mixin/recipe/TippedArrowRecipeMixin.java
+++ src/main/java/de/pnku/mstv_mweaponv/mixin/recipe/TippedArrowRecipeMixin.java
@@ -1,19 +1,20 @@
 package de.pnku.mstv_mweaponv.mixin.recipe;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.core.component.DataComponents;
 import net.minecraft.world.inventory.CraftingContainer;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.Items;
 import net.minecraft.world.item.alchemy.PotionContents;
 import net.minecraft.world.item.crafting.CraftingBookCategory;
 import net.minecraft.world.item.crafting.CustomRecipe;
 import net.minecraft.world.item.crafting.TippedArrowRecipe;
-import net.minecraft.world.level.Level;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Slice;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import static de.pnku.mstv_mweaponv.item.MoreWeaponVariantItems.more_tippable_arrows;
@@ -24,47 +25,37 @@ public abstract class TippedArrowRecipeMixin extends CustomRecipe {
         super(category);
     }
 
-    @Inject(method = "matches*", at = @At("HEAD"), cancellable = true)
-    private void injectedMatches(CraftingContainer craftingContainer, Level level, CallbackInfoReturnable<Boolean> cir) {
-        if (more_tippable_arrows.containsKey(craftingContainer.getItem(0).getItem())) {
-            if (craftingContainer.getWidth() == 3 && craftingContainer.getHeight() == 3) {
-                for (int i = 0; i < craftingContainer.getWidth(); ++i) {
-                    for (int j = 0; j < craftingContainer.getHeight(); ++j) {
-                        ItemStack itemStack = craftingContainer.getItem(i + j * craftingContainer.getWidth());
-                        if (itemStack.isEmpty()) {
-                            cir.setReturnValue(false);
-                        }
-
-                        if (i == 1 && j == 1) {
-                            if (!itemStack.is(Items.LINGERING_POTION)) {
-                                cir.setReturnValue(false);
-                            }
-                        } else if (!more_tippable_arrows.containsKey(itemStack.getItem())) {
-                            cir.setReturnValue(false);
-                        }
-                    }
-                }
-
-                cir.setReturnValue(true);
-            } else {
-                cir.setReturnValue(false);
-            }
-        }
+    // by Linguardium
+    @WrapOperation(
+            method="matches(Lnet/minecraft/world/inventory/CraftingContainer;Lnet/minecraft/world/level/Level;)Z",
+            slice = @Slice(
+                    from=@At(
+                            value="FIELD",
+                            target="Lnet/minecraft/world/item/Items;ARROW:Lnet/minecraft/world/item/Item;"
+                    )
+            ),
+            at= @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/world/item/ItemStack;is(Lnet/minecraft/world/item/Item;)Z",
+                    ordinal = 0
+            )
+    )
+    private boolean wrappedMatchesStackIsArrow(ItemStack instance, Item ARROW, Operation<Boolean> original) {
+        return original.call(instance,ARROW) || more_tippable_arrows.containsKey(instance.getItem());
     }
 
-    @Inject(method = "assemble*", at = @At("HEAD"), cancellable = true)
-    private void injectAssemble(CraftingContainer craftingContainer, HolderLookup.Provider provider, CallbackInfoReturnable<ItemStack> cir) {
-        Item tippableArrowItem = craftingContainer.getItem(0).getItem();
-        if (more_tippable_arrows.containsKey(tippableArrowItem)) {
-            ItemStack lingeringPotionStack = craftingContainer.getItem(1 + craftingContainer.getWidth());
-            if (!lingeringPotionStack.is(Items.LINGERING_POTION)) {
-                cir.setReturnValue(ItemStack.EMPTY);
-            } else {
-                Item tippedArrowItem = more_tippable_arrows.get(tippableArrowItem);
-                ItemStack tippedArrowStack = new ItemStack(tippedArrowItem, 8);
-                tippedArrowStack.set(DataComponents.POTION_CONTENTS, (PotionContents) lingeringPotionStack.get(DataComponents.POTION_CONTENTS));
-                cir.setReturnValue(tippedArrowStack);
-            }
-        }
+    @Inject(
+            method = "assemble(Lnet/minecraft/world/inventory/CraftingContainer;Lnet/minecraft/core/HolderLookup$Provider;)Lnet/minecraft/world/item/ItemStack;",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/world/item/ItemStack;<init>(Lnet/minecraft/world/level/ItemLike;I)V"
+            ),
+            cancellable = true)
+    private void injectedAssemble(CraftingContainer input, HolderLookup.Provider registries, CallbackInfoReturnable<ItemStack> cir){
+        Item tippedArrowVariantItem = more_tippable_arrows.get(input.getItem(0).getItem());
+        ItemStack tippedArrowStack = new ItemStack(tippedArrowVariantItem, 8);
+        ItemStack lingeringPotionStack = input.getItem(4);
+        tippedArrowStack.set(DataComponents.POTION_CONTENTS, (PotionContents) lingeringPotionStack.get(DataComponents.POTION_CONTENTS));
+        cir.setReturnValue(tippedArrowStack);
     }
 }
