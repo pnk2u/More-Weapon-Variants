690eb1abfb0042b16768127c942ffd39f081def9
pnku <pnku@pnku.de>
2026-04-06 02:18:22 +0200

    Add missing import (1.5.5-d:c2+1.21.4:c1)

--- src/main/java/de/pnku/mstv_mweaponv/item/MoreWeaponVariantItems.java
+++ src/main/java/de/pnku/mstv_mweaponv/item/MoreWeaponVariantItems.java
@@ -1,7 +1,6 @@
 package de.pnku.mstv_mweaponv.item;
 
 import de.pnku.mstv_base.item.MoreStickVariantItem;
-import de.pnku.mstv_mweaponv.MoreWeaponVariants;
 import net.minecraft.core.component.DataComponents;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.core.registries.Registries;
@@ -15,6 +14,7 @@ import java.util.*;
 
 import static de.pnku.mstv_base.item.MoreStickVariantItems.*;
 
+import static de.pnku.mstv_mweaponv.MoreWeaponVariants.withModId;
 import static net.minecraft.world.item.ToolMaterial.*;
 
 public class MoreWeaponVariantItems {
@@ -429,6 +429,6 @@ public class MoreWeaponVariantItems {
     private static void registerWeaponItem(Item weaponItem, Item stickIngredient, String weaponName) {
         more_weapons.add(weaponItem);
         more_weapon_sticks.put(weaponItem, stickIngredient);
-        Registry.register(BuiltInRegistries.ITEM, MoreWeaponVariants.withModId(weaponName), weaponItem);
+        Registry.register(BuiltInRegistries.ITEM, withModId(weaponName), weaponItem);
     }
 }
