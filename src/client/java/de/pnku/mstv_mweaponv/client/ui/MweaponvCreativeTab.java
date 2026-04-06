5843ee248ea35255f710bbc6978f51e3a07bab39
pnku <pnku@pnku.de>
2026-04-06 02:32:09 +0200

    Adapt merged changes' mapping to 1.21.11 (1.5.5-d:c2+.4:c2+.9(10):c1+1.21.11:c1)

--- src/client/java/de/pnku/mstv_mweaponv/client/ui/MweaponvCreativeTab.java
+++ src/client/java/de/pnku/mstv_mweaponv/client/ui/MweaponvCreativeTab.java
@@ -6,7 +6,7 @@ import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.core.Registry;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.network.chat.Component;
-import net.minecraft.resources.ResourceLocation;
+import net.minecraft.resources.Identifier;
 import net.minecraft.world.item.*;
 
 import java.util.List;
@@ -26,7 +26,7 @@ public class MweaponvCreativeTab extends CreativeModeTabs {
                 if (more_swords.contains(weaponItem) && MoreWeaponVariants.isMtoolvLoaded) {
                     String axeItemName = BuiltInRegistries.ITEM.getKey(weaponItem).getPath().replace("sword", "axe");
                     String axeItemNamespace = BuiltInRegistries.ITEM.getKey(weaponItem).getNamespace().replace("mstv-mweaponv", "mstv-mtoolv");
-                    entries.accept(BuiltInRegistries.ITEM.get(ResourceLocation.tryBuild(axeItemNamespace, axeItemName)).get().value());
+                    entries.accept(BuiltInRegistries.ITEM.get(Identifier.tryBuild(axeItemNamespace, axeItemName)).get().value());
                 }
             }
         }
