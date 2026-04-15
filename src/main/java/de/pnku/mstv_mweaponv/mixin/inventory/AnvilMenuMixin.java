de743d1f25aea4c4ec7b5d12bfaf21acf62342d0
pnku <pnku@pnku.de>
2026-04-15 23:25:51 +0200

    Adapt AnvilMenuMixin to 1.21.4 (1.5.6-d:c6+1.21.4:c1)

--- src/main/java/de/pnku/mstv_mweaponv/mixin/inventory/AnvilMenuMixin.java
+++ src/main/java/de/pnku/mstv_mweaponv/mixin/inventory/AnvilMenuMixin.java
@@ -2,10 +2,13 @@ package de.pnku.mstv_mweaponv.mixin.inventory;
 
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import net.minecraft.core.component.DataComponents;
 import net.minecraft.world.inventory.AnvilMenu;
+import net.minecraft.world.item.DiggerItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.TieredItem;
+import net.minecraft.world.item.SwordItem;
+import net.minecraft.world.item.enchantment.Repairable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 
@@ -20,15 +23,15 @@ public abstract class AnvilMenuMixin {
 
         Item firstInputItem = firstInputStack.getItem();
 
-        if (!(firstInputItem instanceof TieredItem firstTiered) || !(secondInputItem instanceof TieredItem secondTiered)) {
+        if (!(firstInputItem instanceof SwordItem || firstInputItem instanceof DiggerItem)
+                || !(secondInputItem instanceof SwordItem || secondInputItem instanceof DiggerItem)
+                || firstInputItem.getClass() != secondInputItem.getClass()) {
             return false;
         }
 
-        // Require same tool class to avoid cross-type merges (e.g., sword + axe).
-        if (firstInputItem.getClass() != secondInputItem.getClass()) {
-            return false;
-        }
+        Repairable firstRepairable = firstInputStack.get(DataComponents.REPAIRABLE);
+        Repairable secondRepairable = secondInputItem.getDefaultInstance().get(DataComponents.REPAIRABLE);
 
-        return firstTiered.getTier() == secondTiered.getTier();
+        return firstRepairable != null && firstRepairable.equals(secondRepairable);
     }
 }
